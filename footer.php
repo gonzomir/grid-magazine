a386e1bf6b8359370e406e8fb34e6b0309b2defe
Milen Petrinski <gonzo@greatgonzo.net>
2017-05-07 14:36:46 +0300

    Initial commit. Start with _s as a base.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Grid_Magazine
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'grid-mag' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'grid-mag' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'grid-mag' ), 'grid-mag', '<a href="https://automattic.com/" rel="designer">Milen Petrinski - Gonzo</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
