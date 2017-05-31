0085283d51366f11478c1f5be35783d2b5880659
Milen Petrinski <gonzo@greatgonzo.net>
2017-05-31 13:37:45 +0300

    Header tweaks - site title font size for small screens, menu toggle button label, whitespace.

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'grid-mag' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'grid-mag' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu', 'container' => 'ul', 'items_wrap'     => '<ul>%3$s</ul>' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
