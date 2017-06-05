e2176f9b2d0710f585be794d8d938e85858cb9df
Milen Petrinski <gonzo@greatgonzo.net>
2017-06-05 18:22:53 +0300

    Single post page layout.

---
+++ template-parts/content-single.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Grid_Magazine
+ */
+
+
+global $wp_query;
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php if ( has_post_thumbnail() ) : ?>
+	<div class="post-thumbnail">
+		<?php
+		$thumbnail_size = 'gridmag-wide-medium';
+		$sizes = '(min-width: 60em) calc( 100vw - (2.5em + (100vw - 60em) / 5) * 2 ), (min-width: 40em) calc(100vw - (1.5em + (100vw - 40em) / 20) * 2), calc(100vw - 3em)';
+		?>
+		<?php the_post_thumbnail( $thumbnail_size, array( 'sizes' => $sizes ) ); ?>
+	</div>
+	<?php endif; ?>
+
+	<header class="entry-header">
+		<?php
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+		?>
+	</header><!-- .entry-header -->
+
+	<?php if ( 'post' === get_post_type() ) : ?>
+	<footer class="entry-meta">
+		<p>
+		<?php grid_mag_posted_on(); ?>
+		<?php grid_mag_entry_footer(); ?>
+		</p>
+	</footer><!-- .entry-meta -->
+	<?php endif; ?>
+
+	<div class="entry-content">
+		<?php
+			if ( is_single() ) {
+
+				the_content();
+
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'grid-mag' ),
+					'after'  => '</div>',
+				) );
+
+			}
+			else {
+				the_excerpt();
+			}
+
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
