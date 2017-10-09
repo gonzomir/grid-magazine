9d668aef0c80c9590c57475a0463d347bffb5d82
Milen Petrinski <gonzo@greatgonzo.net>
2017-10-09 12:06:35 +0300

    Escape properly theme copy.

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'grid-magazine' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'grid-magazine' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'grid-magazine' ), sprintf( '<a href="https://github.com/gonzomir/grid-magazine">%s</a>', __( 'Grid Magazine', 'grid-magazine' ) ), __( 'Milen Petrinski - Gonzo', 'grid-magazine' ) ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'grid-magazine' ), sprintf( '<a href="https://github.com/gonzomir/grid-magazine">%s</a>', esc_html__( 'Grid Magazine', 'grid-magazine' ) ), esc_html__( 'Milen Petrinski - Gonzo', 'grid-magazine' ) ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-  <label for="s" class="screen-reader-text"><?php _e( 'Search for:', 'grid-magazine' ); ?></label>
+  <label for="s" class="screen-reader-text"><?php esc_html_e( 'Search for:', 'grid-magazine' ); ?></label>
   <input type="search" name="s" id="s" class="search-field" placeholder="<?php echo esc_attr( __( 'Search &hellip;', 'grid-magazine' ) ); ?>" value="<?php get_search_query(); ?>" required />
-  <button type="submit"><?php _e( 'Search', 'grid-magazine' ); ?></button>
+  <button type="submit"><?php esc_html_x( 'Search', 'search button label', 'grid-magazine' ); ?></button>
 </form>
